43b4d7b58c564a1bb55eec53bd72efe86e7924ac
agomesd <alex.gduguay@gmail.com>
2025-01-27 14:24:37 +0000

    Refactor into separate modules and add config file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::net::TcpListener;
 
-use email_newsletter::run;
+use email_newsletter::{configuration::get_configuration, startup::run};
+
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
    run(listener)?.await
 }
