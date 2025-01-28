ce8028d83991ae946a53a98db0314f86c99e6ff8
agomesd <alex.gduguay@gmail.com>
2025-01-28 16:45:24 +0000

    Use PgPool instead of PgConnection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
-
-use email_newsletter::{configuration::get_configuration, startup::run};
 
 
 
@@ -9,7 +10,10 @@ use email_newsletter::{configuration::get_configuration, startup::run};
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    .await
+    .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-   run(listener)?.await
+   run(listener, connection_pool)?.await
 }
